f3d21520358f4c367f5d587957e5f873e9bcea14
Vitor Dal Pra <vitordalpra@gmail.com>
2020-11-18 14:21:26 -0300

    routes completed, controller and views of restaurant, wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 5 restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+    )
+  restaurant.save!
+end
+puts 'Finished!'
